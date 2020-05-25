d1ad99e0ec593bda06d5e0e3eacb2c016f0e6f50
bpipkin <pipkin.benjamin@gmail.com>
2020-05-25 13:01:57 -0400

    working version with reply

--- acnhturnips.py
+++ acnhturnips.py
@@ -15,7 +15,7 @@ if not os.path.isfile("logs/acnhturnips/posts_replied_to.txt"):
 # If we have run the code before, load the list of posts we have replied to
 else:
     # Read the file into a list and remove any empty values
-    with open("logs/acnhturnips//posts_replied_to.txt", "r") as f:
+    with open("logs/acnhturnips/posts_replied_to.txt", "r") as f:
         posts_replied_to = f.read()
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
@@ -29,18 +29,20 @@ for submission in subreddit.new(limit=10):
     if submission.id not in posts_replied_to:
 
         # Do a case insensitive search
-        if re.search("nook", submission.title, re.IGNORECASE):
-            numString = ''
-            for i in range(0,len(submission.title)):
-                if isinstance(submission.title[i],int) and len(numString) < 3:
-                    numString += submission.title[i]
+        if re.search('nook', submission.title, re.IGNORECASE):
+            num = 0
+            mult = 1
+            for i in reversed(submission.title):
+                if i.encode('utf-8') == '0' or i.encode('utf-8') == '1' or i.encode('utf-8') == '2' or i.encode('utf-8') == '3' or i.encode('utf-8') == '4' or i.encode('utf-8') == '5' or i.encode('utf-8') == '6' or i.encode('utf-8') == '7' or i.encode('utf-8') == '8' or i.encode('utf-8') == '9':
+                    num += int(i.encode('utf-8')) * mult
+                    mult = mult * 10
 
-            if len(numString) == 3 and int(numString) > 500:
+            if num > 499:
+                print submission.title.encode('utf-8').strip()
+                print num
+
                 # Reply to the post
-                # submission.reply("I am interested! Can I please get your dodo code?");
-
-                print submission.title
-                print numString
+                submission.reply("I am interested! Can I please get your dodo code?");
 
                 # Store the current id into our list
                 posts_replied_to.append(submission.id)
